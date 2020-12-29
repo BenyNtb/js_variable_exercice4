38fc61d80f930eec800ce9af67ce3e7e5d397520
Beny <b.ntambwe97@gmail.com>
2020-12-29 09:14:33 +0100

    fin exo

---
+++ public/js/main.js
@@ -0,0 +1,18 @@
+// let nom = "Ntb"
+// let prenom = "Beny"
+// var reponse = prompt ("Where are you from ?")
+// alert(nom + " " + prenom + " " + reponse)
+
+//EXO 2
+
+let nom = "Ntb"
+let prenom = "Beny"
+let depart = "Bruxelles"
+let destination = "New York"
+var reponse1 = prompt ("From")
+var reponse2 = prompt ("To")
+var reponse3 = prompt ("What's your name ?")
+var reponse4 = prompt ("What's your first name ?")
+alert(`Le voyage de ${depart} vers ${destination} a été enregistré au nom de ${nom} ${prenom}`)
+let result = reponse1 + reponse2 + reponse3 + reponse4
+console.log(`${reponse1} + ${reponse2} + ${reponse3} + ${reponse4}`)
